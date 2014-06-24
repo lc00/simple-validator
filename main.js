26fbde0d7d4d77be7684af66d116133f732abdc4
lc00 <leannecheung00@gmail.com>
2014-06-23 18:25:21 -0600

    done with phone-number section

--- main.js
+++ main.js
@@ -1,38 +1,27 @@
 
 
-var phoneNum = prompt("hey, what's your phone number; please " +'< /br>'+ 
-	"type it in xxx-xxx-xxxx format");
-
-
-
-
-while (phoneNum.length !== 12){
-	phoneNum = prompt('please make sure that you are entering the entire number');
-}
-
-while (phoneNum[3] !== '-' || phoneNum[7] !== '-') {
-	phoneNum = prompt('please add dashes');
-}
-
-phoneNumCopy = phoneNum.split('');
-phoneNumCopy.splice(3,1);
-phoneNumCopy.splice(6,1);
-
-console.log(phoneNumCopy);
-
 
 function isNum(element, index, array){
 	// console.log(element);
 	return (element<=9 && element>=0);
 }
+var phoneNumCopy, passed;
 
-var passed = phoneNumCopy.every(isNum);
+var phoneNum = prompt("hey, what's your phone number; please " + '< /br>'+ 
+	"type it in xxx-xxx-xxxx format");
 
-while ( passed !== true ){
-	prompt('please enter only numbers');
+
+while (phoneNum.length !== 12 || (phoneNum[3] !== '-' || phoneNum[7] !== '-') || passed !== true ){ 
+	phoneNum = prompt('invalid entry; try again');
+	phoneNumCopy = phoneNum.split('');
+	phoneNumCopy.splice(3,1);
+	phoneNumCopy.splice(6,1);
+	passed = phoneNumCopy.every(isNum);
+
 }
 
 
-// var birthDate = prompt('what's your birthday);
-// if (birthDate[2] !== '/' && birthDate[5] !== '/')
-// 	birthDate = prompt('invalid birthday format, please add '/' ');
+
+var birthDate = prompt('what's your birthday);
+if (birthDate[2] !== '/' && birthDate[5] !== '/')
+	birthDate = prompt('invalid birthday format, please add '/' ');
