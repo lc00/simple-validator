fd582714332f274246eeb9eaf717a9322665b63e
lc00 <leannecheung00@gmail.com>
2014-06-23 21:37:41 -0600

    finished, but need testing

--- main.js
+++ main.js
@@ -5,40 +5,42 @@ function isNum(element, index, array){
 	// console.log(element);
 	return (element<=9 && element>=0);
 }
-var copy, passed;
 
-var phoneNum = prompt("hey, what's your phone number; please " + '< /br>'+ 
-	"type it in xxx-xxx-xxxx format");
+var phoneNum, copy, passed;
+var birthDate, state, married;
 
-copy = phoneNum.split('');
-copy.splice(3,1);
-copy.splice(6,1);
-passed = copy.every(isNum);
+do{
+	phoneNum = prompt("hey, what's your phone number; please " + 
+		"type it in xxx-xxx-xxxx format");
 
-while (phoneNum.length !== 12 || (phoneNum[3] !== '-' || phoneNum[7] !== '-') || passed !== true ){ 
-	phoneNum = prompt('invalid entry; try again');
 	copy = phoneNum.split('');
 	copy.splice(3,1);
 	copy.splice(6,1);
 	passed = copy.every(isNum);
+	}while (phoneNum.length !== 12 || (phoneNum[3] !== '-' || phoneNum[7] !== '-') || passed !== true );
 
-}
+alert("valid phone number");
 
 
+do{
+	birthDate = prompt("what's your birthday");
 
-
-var birthDate = prompt("what's your birthday");
-
-copy = birthDate.split('');
-copy.splice(2,1);
-copy.splice(4,1);
-passed = copy.every(isNum);
-
-while (birthDate.length !== 6 || (birthDate[2] !== '/' || birthDate[5] !== '/') || passed !== true) {
-	birthDate = prompt("invalid birthday format, try again");
-	copy = phoneNum.split('');
+	copy = birthDate.split('');
 	copy.splice(2,1);
-	copy.splice(5,1);
+	copy.splice(4,1);
 	passed = copy.every(isNum);
 
-}
+}while (birthDate.length !== 8 || (birthDate[2] !== '/' || birthDate[5] !== '/') || passed !== true);
+
+alert("valid birth date");
+
+
+do{
+	state = prompt("What is the abbreviation of your state");
+	stateCap = state.toUpperCase();
+} while (state.length !== 2 || state !== stateCap);
+
+do{
+	married = prompt("Are you married");
+
+} while( married !== "YES" || married !=="Yes" || married !=="yes" || married !== "no" );
