63b731f4df0bc9553417b2011dc898f638fae680
lc00 <leannecheung00@gmail.com>
2014-06-23 19:11:57 -0600

    almost done with birthday

--- main.js
+++ main.js
@@ -5,23 +5,40 @@ function isNum(element, index, array){
 	// console.log(element);
 	return (element<=9 && element>=0);
 }
-var phoneNumCopy, passed;
+var copy, passed;
 
 var phoneNum = prompt("hey, what's your phone number; please " + '< /br>'+ 
 	"type it in xxx-xxx-xxxx format");
 
+copy = phoneNum.split('');
+copy.splice(3,1);
+copy.splice(6,1);
+passed = copy.every(isNum);
 
 while (phoneNum.length !== 12 || (phoneNum[3] !== '-' || phoneNum[7] !== '-') || passed !== true ){ 
 	phoneNum = prompt('invalid entry; try again');
-	phoneNumCopy = phoneNum.split('');
-	phoneNumCopy.splice(3,1);
-	phoneNumCopy.splice(6,1);
-	passed = phoneNumCopy.every(isNum);
+	copy = phoneNum.split('');
+	copy.splice(3,1);
+	copy.splice(6,1);
+	passed = copy.every(isNum);
 
 }
 
 
 
-var birthDate = prompt('what's your birthday);
-if (birthDate[2] !== '/' && birthDate[5] !== '/')
-	birthDate = prompt('invalid birthday format, please add '/' ');
+
+var birthDate = prompt("what's your birthday");
+
+copy = birthDate.split('');
+copy.splice(2,1);
+copy.splice(4,1);
+passed = copy.every(isNum);
+
+while (birthDate.length !== 6 || (birthDate[2] !== '/' || birthDate[5] !== '/') || passed !== true) {
+	birthDate = prompt("invalid birthday format, try again");
+	copy = phoneNum.split('');
+	copy.splice(2,1);
+	copy.splice(5,1);
+	passed = copy.every(isNum);
+
+}
